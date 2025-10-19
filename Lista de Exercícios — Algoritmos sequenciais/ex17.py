6cd3f8926197689a26b0aaf5cc0f5655dff3568e
Eduardo Flores <eduardofloresdev@gmail.com>
2025-10-19 10:27:21 -0300

    Exercício 17

---
+++ Lista de Exercícios — Algoritmos sequenciais/ex17.py
@@ -0,0 +1,8 @@
+# Questão 17. Elabore um algoritmo que lê dois valores inteiros e mostra o produto dos valores.
+
+valor_1 = int(input("Digite um valor inteiro: "))
+valor_2 = int(input("Digite outro valor inteiro: "))
+
+produto = valor_1 * valor_2
+
+print(f"O produto de {valor_1} por {valor_2} é de: {produto}")
