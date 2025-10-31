f1f18ac660215beba495f199b35fbd151ed7a9a7
Eduardo Flores <eduardofloresdev@gmail.com>
2025-10-30 21:45:05 -0300

    Exercício 26

---
+++ Lista de Exercícios — Algoritmos sequenciais/ex26.py
@@ -0,0 +1,12 @@
+# Questão 26. Mostrar a média aritmética entre 3 números informados pelo usuário.
+
+# ENTRADA
+primeiro_numero = int(input("Digite o primeiro número: "))
+segundo_numero = int(input("Digite o segundo número: "))
+terceir_numero = int(input("Digite o terceiro número: "))
+
+# PROCESSAMENTO
+media = (primeiro_numero + segundo_numero + terceir_numero) / 3
+
+# SAÍDA
+print(f"A média aritmética dos três valores digitados é de: {media}")
