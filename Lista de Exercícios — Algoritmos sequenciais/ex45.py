a6332c3145b0f94618d17ecacdcc29b3b49bc352
Eduardo Flores <eduardofloresdev@gmail.com>
2025-10-31 23:09:03 -0300

    Exercício 45

---
+++ Lista de Exercícios — Algoritmos sequenciais/ex45.py
@@ -0,0 +1,22 @@
+# Questão 45. Uma fabrica de camisetas produz os tamanhos pequeno, médio e grande, cada uma
+# sendo vendida respectivamente por 10, 12 e 15 reais. Construa um algoritmo em que o usuário
+# forneça a quantidade de camisetas pequenas, medias e grandes referentes a uma venda, e a
+# maquina informe quanto será o valor arrecadado.
+
+# ENTRADA
+PRECO_CAMISA_P = 10
+PRECO_CAMISA_M = 12
+PRECO_CAMISA_G = 15
+
+qntd_camisas_p = int(input("Digite a quantidade de camisas p compradas: "))
+qntd_camisas_m = int(input("Digite a quantidade de camisas m compradas: "))
+qntd_camisas_g = int(input("Digite a quantidade de camisas g compradas: "))
+
+# PROCESSAMENTO
+total_camisas_p = qntd_camisas_p * PRECO_CAMISA_P
+total_camisas_m = qntd_camisas_m * PRECO_CAMISA_M
+total_camisas_g = qntd_camisas_g * PRECO_CAMISA_G
+total = total_camisas_p + total_camisas_m + total_camisas_g
+
+# SAÍDA
+print(f"O valor total arrecadodo com a venda das camisas é de: R$ {total}")
