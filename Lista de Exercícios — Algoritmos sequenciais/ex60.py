89f6db48ce495c6f0f1309b581b224adbfc1d3b0
Eduardo Flores <eduardofloresdev@gmail.com>
2025-11-02 20:47:06 -0300

    Exercício 60

---
+++ Lista de Exercícios — Algoritmos sequenciais/ex60.py
@@ -0,0 +1,10 @@
+# Questão 60. Faça um programa que passa temperatura em celsius e mostra em fahrenheit.
+
+# ENTRADA
+C = float(input("Passe a temperatura em Celcius: "))
+
+# PROCESSAMENTO
+F = (C * 1.8) + 32
+
+# SAÍDA
+print(f"A temperatura de {C}°C em Fahrenheit é de: {F}°F")
