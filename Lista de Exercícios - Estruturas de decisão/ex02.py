059550bf3b3e83a7ab70f69f7169cc34a8ccf78e
Eduardo Flores <eduardofloresdev@gmail.com>
2025-11-12 20:58:59 -0300

    Exercício 02

---
+++ Lista de Exercícios - Estruturas de decisão/ex02.py
@@ -0,0 +1,10 @@
+# Questão 2. Faça um algoritmo que leia um número e mostrar se ele é positivo ou negativo.
+
+# ENTRADA
+numero = int(input("Digite um número: "))
+
+# PROCESSAMENTO
+if numero >= 0:
+    print(f"O número {numero} é positivo")
+else:
+    print(f"O número {numero} é negativo")
