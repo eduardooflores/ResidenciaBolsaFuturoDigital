1fcf196b18f20bb4bf21d851ee4ff61ae5658556
Eduardo Flores <eduardofloresdev@gmail.com>
2025-11-12 18:37:13 -0300

    Exercício 08

---
+++ Lista de Exercícios - Estruturas de decisão/ex08.py
@@ -0,0 +1,30 @@
+# Questão 8. Faça um algoritmo que leia três valores e indica quantos são pares e quantos são
+# ímpares.
+
+contador_par = 0
+contador_impar = 0
+
+# ENTRADA
+primeiro_valor = int(input("Digite o primeiro valor: "))
+segundo_valor = int(input("Digite o segundo valor: "))
+terceiro_valor = int(input("Digite o terceiro valor: "))
+
+# PROCESSAMENTO
+if primeiro_valor % 2 == 0:
+    contador_par += 1
+else:
+    contador_impar += 1
+
+if segundo_valor % 2 == 0:
+    contador_par += 1
+else:
+    contador_impar += 1
+
+if terceiro_valor % 2 == 0:
+    contador_par += 1
+else:
+    contador_impar += 1
+
+# SAÍDA
+print(f"Existem {contador_par} números PAR entre os números")
+print(f"Existem {contador_impar} números ÍMPAR entre os números")
