8e6908a43933356ecd4d4cd1c04e4588f3c7ea3f
Eduardo Flores <eduardofloresdev@gmail.com>
2025-11-21 20:44:14 -0300

    Exercício 01

---
+++ Lista de Exercícios Condicionais - Definições/ex01.py
@@ -0,0 +1,24 @@
+# ENTRADA
+ddd = int(input("Digite o DDD: "))
+
+# PROCESSAMENTO
+if ddd == 51:
+    print("Porto Alegre")
+elif ddd == 61:
+    print("Salvador")
+elif ddd == 11:
+    print("São Paulo")
+elif ddd == 21:
+    print("Rio de Janeiro")
+elif ddd == 31:
+    print("Salvador")
+elif ddd == 61:
+    print("Belo Horizonte")
+elif ddd == 54:
+    print("Caxias do Sul")
+elif ddd == 55:
+    print("Santa Maria")
+elif ddd == 53:
+    print("Pelotas")
+else:
+    print("DDD não cadastrado")
