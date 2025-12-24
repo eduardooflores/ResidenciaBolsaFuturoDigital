0f545ee13c82c71d6bcff4f6457f288d870e090e
Eduardo Flores <eduardofloresdev@gmail.com>
2025-12-23 23:40:24 -0300

    Exercicio 05

---
+++ Lista de Exercícios - Estruturas de decisão/ex05.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número para descobrir se ele é maior, menor ou igual a 20: "))
+
+if numero > 20:
+    print("Maior do que 20!")
+elif numero < 20:
+    print("Menor do que 20!")
+else:
+    print("O número é 20!")
