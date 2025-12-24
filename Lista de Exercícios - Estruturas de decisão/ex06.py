0f2f6557049c49c624318f0a64d91f591d933bbf
Eduardo Flores <eduardofloresdev@gmail.com>
2025-12-23 23:54:29 -0300

    Exercicio 06

---
+++ Lista de Exercícios - Estruturas de decisão/ex06.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+soma = n1 + n2
+
+if soma > 10:
+    print(f"SOMA: {n1} + {n2} = {soma}")
+else:
+    print(f"Valor do primeiro número:{n1}")
+    print(f"Valor do segundo número: {n2}")
