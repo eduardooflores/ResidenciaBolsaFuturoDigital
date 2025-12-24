52f30275dd9dd40e8c9b9440a6ce7a2ac0cb6e0f
Eduardo Flores <eduardofloresdev@gmail.com>
2025-12-24 00:16:03 -0300

    Exercicio 07

---
+++ Lista de Exercícios - Estruturas de decisão/ex07.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite o primeiro valor: "))
+n2 = int(input("Digite o segundo valor: "))
+n3 = int(input("Digite o terceiro valor: "))
+soma = n1 + n2 + n3
+
+if soma % 2 == 0:
+    print("A soma dos valores é PAR")
+else:
+    print("A soma dos valores é ÍMPAR")
