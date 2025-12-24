c0b64a3430e50482775f2efb77fa6a8431cda1e3
Eduardo Flores <eduardofloresdev@gmail.com>
2025-12-24 15:35:21 -0300

    Alteracoes exercicio 16

--- Lista de Exercícios - Estruturas de decisão/ex16.py
+++ Lista de Exercícios - Estruturas de decisão/ex16.py
@@ -1,6 +1,6 @@
 numero = int(input("Digite um número: "))
 
-if numero % 5 == 0 or numero % 9 == 0:
-    print(f"O número {numero} é divisível por 5 ou 9")
+if numero % 3 == 0 and numero % 7 == 0:
+    print(f"O número {numero} é divisível por 3 e por 7")
 else:
-    print(f"O número {numero} não é divisível por 5 nem por 9")
+    print(f"O número {numero} não é divisível por 3 e 7")
