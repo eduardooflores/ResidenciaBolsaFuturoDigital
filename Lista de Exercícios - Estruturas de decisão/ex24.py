30aeddc517768dd88bd2a08864eadaa7bf7a2597
Eduardo Flores <eduardofloresdev@gmail.com>
2025-12-25 02:10:07 -0300

    Exercicio 24

---
+++ Lista de Exercícios - Estruturas de decisão/ex24.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número inteiro: "))
+
+if numero >= 20 and numero <= 30:
+    print(f"O número {numero} está entre 20 e 30")
+elif numero >= 50 and numero <= 60:
+    print(f"O número {numero} está entre 50 e 60")
+else:
+    print(f"O número {numero} está fora dos intervalos")
