a726dff5041a9590af1517b0465e9d7b1ea74b60
Eduardo Flores <eduardofloresdev@gmail.com>
2025-12-25 01:57:21 -0300

    Exercicio 23

---
+++ Lista de Exercícios - Estruturas de decisão/ex23.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um número inteiro: "))
+
+if numero >= 20 and numero <= 90:
+    print(f"O número {numero} está entre 20 e 90.")
+else:
+    print(f"O número {numero} não está entre 20 e 90.")
