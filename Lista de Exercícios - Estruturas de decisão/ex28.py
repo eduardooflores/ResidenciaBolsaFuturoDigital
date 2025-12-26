18fe66010175963e43fb936a07a48cc87056237d
Eduardo Flores <eduardofloresdev@gmail.com>
2025-12-26 19:34:51 -0300

    Alteracoes no ex28

--- Lista de Exercícios - Estruturas de decisão/ex28.py
+++ Lista de Exercícios - Estruturas de decisão/ex28.py
@@ -1,15 +1,11 @@
 idade = int(input("Digite sua idade: "))
 peso = float(input("Digite seu peso: "))
 jejum = input("Está em jejum ? (1.Sim 2.Não): ")
-documento = input("Possuí documento com foto ? (1.Sim 2.Não): ")
 
 if idade >= 18 and idade <= 67:
     if peso > 50:
         if jejum == "1":
-            if documento == "1":
                 print("Pode doar sangue!")
-            else:
-                print("Não pode doar sangue! Precisa possuir documento com foto!")
         else:
             print("Não pode doar sangue! Precisa estar em jejum!")
     else:
