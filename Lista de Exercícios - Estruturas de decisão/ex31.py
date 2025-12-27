4d6b25421239cc46f6ca8fee309d15150316d21e
Eduardo Flores <eduardofloresdev@gmail.com>
2025-12-27 16:57:23 -0300

    Exercicio 31

---
+++ Lista de Exercícios - Estruturas de decisão/ex31.py
@@ -0,0 +1,26 @@
+idade = int(input("Digite a sua idade: "))
+peso = float(input("Digite o seu peso: "))
+jejum = input("Está em jejum ? (1.Sim 2.Não): ")
+documento = input("Possui documento com foto ? (1.Sim 2.Não): ")
+hepatite = input("Você teve hepatite após os 10 anos? (1.Sim 2.Não): ")
+malaria = input("Você teve malária? (1.Sim 2.Não): ")
+
+if idade >= 18 and idade <= 67:
+    if peso > 50:
+        if jejum == "1":
+            if documento == "1":
+                if hepatite == "2":
+                    if malaria == "2":
+                        print("Pode doar sangue! Todos os requisitos aceitos")
+                    else:
+                        print("Não pode doar sangue! Malária impossibilita a doação de sangue")
+                else:
+                    print("Não pode doar sangue! Hepatite após os 10 anos")
+            else:
+                print("Não pode doar sangue! Deve possuir documento com foto")
+        else:
+            print("Não pode doar sangue! Deve estar em jejum")
+    else:
+        print("Não pode doar sangue! Peso inválido")
+else:
+    print("Não pode doar sangue! Idade inválida")
