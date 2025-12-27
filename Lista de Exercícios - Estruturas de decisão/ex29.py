6c3c0a8052f509097ab1cac5bac2d514b20f5ddc
Eduardo Flores <eduardofloresdev@gmail.com>
2025-12-27 16:52:06 -0300

    Correcao de portugues ex29

--- Lista de Exercícios - Estruturas de decisão/ex29.py
+++ Lista de Exercícios - Estruturas de decisão/ex29.py
@@ -1,7 +1,7 @@
 idade = int(input("Digite sua idade: "))
 peso = float(input("Digite seu peso: "))
 jejum = input("Está em jejum ? (1.Sim 2.Não): ")
-documento = input("Possuí documento com foto ? (1.Sim 2.Não): ")
+documento = input("Possui documento com foto ? (1.Sim 2.Não): ")
 
 if idade >= 18 and idade <= 67:
     if peso > 50:
