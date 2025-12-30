18af69631d8728da2cc44a2dd1a527f2a0cf4a7a
Eduardo Flores <eduardofloresdev@gmail.com>
2025-12-29 23:51:33 -0300

    Exercicio 43

---
+++ Lista de Exercícios - Estruturas de decisão/ex43.py
@@ -0,0 +1,8 @@
+idade = int(input("Digite a sua idade: "))
+
+if idade < 18:
+    print("Menor de idade!")
+elif idade > 65:
+    print("Você tem mais de 65 anos!")
+else:
+    print("Maior de idade!")
