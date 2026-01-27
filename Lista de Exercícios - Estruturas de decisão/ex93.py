9da2c5c6fe09df8b875cd45ad9794676f90b789e
Eduardo Flores <eduardofloresdev@gmail.com>
2026-01-27 09:12:40 -0300

    Exercicio 93

---
+++ Lista de Exercícios - Estruturas de decisão/ex93.py
@@ -0,0 +1,8 @@
+idade = int(input("Digite sua idade: "))
+
+if idade > 21:
+    print("Você tem mais de 21 anos.")
+elif idade < 21:
+    print("Você tem menos de 21 anos.")
+else:
+    print("Você tem 21 anos.")
